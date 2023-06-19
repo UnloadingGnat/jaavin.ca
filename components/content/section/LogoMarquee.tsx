2662d295639a0334859dad9026da3fdb8e40ca7d
jaavin <jaavinjm123@gmail.com>
2023-06-19 17:38:05 -0400

    fix: pause on hover disabled on mobile device

--- components/content/section/LogoMarquee.tsx
+++ components/content/section/LogoMarquee.tsx
@@ -1,11 +1,13 @@
 import Image from "next/image";
 import Marquee from "react-fast-marquee";
+import { isMobile } from "react-device-detect";
 
 export default function LogoMarquee() {
+  const pauseHover = () => !isMobile;
   return (
     <>
       <div className="bg-[#222329] bg-opacity-80 backdrop-blur-xl py-3 rounded-3xl">
-        <Marquee pauseOnHover={true} speed={110}>
+        <Marquee pauseOnHover={pauseHover()} speed={110}>
           <Image
             src="/Ipex-logo.svg"
             alt="I Build Cool Things"
