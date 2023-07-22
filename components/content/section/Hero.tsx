a46165cca3933bab963f8fd7ccc39d0ae21e639c
jaavin <jaavinjm123@gmail.com>
2023-07-22 00:21:32 -0400

    feat: change hero text

--- components/content/section/Hero.tsx
+++ components/content/section/Hero.tsx
@@ -6,9 +6,11 @@ export default function Hero() {
           <div className="mx-auto max-w-full lg:mx-0">
             <p className="text-xl lg:text-3xl  leading-7 text-[#999]">Hello!</p>
             <h2 className="mt-2 text-2xl lg:text-4xl font-semibold tracking-normal text-white sm:text-4xl">
-              I&apos;m Jaavin Mohanakumar. <br />
-              Full Stack Developer.
+              I&apos;m Jaavin.
             </h2>
+            <h3 className="text-xl lg:text-3xl tracking-normal text-white sm:text-4xl">
+              Full Stack Developer.
+            </h3>
             <p className="mt-2 text-lg  tracking-tight text-[#999] sm:text-3xl">
               I am a multi-disciplinary software developer who enjoys crafting
               intuitive interfaces. Driven by the desire to solve complex
