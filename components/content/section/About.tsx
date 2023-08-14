f561d26e460a15ad886c2b79fe91e16bec62ae47
jaavin <jaavinjm123@gmail.com>
2023-08-14 14:16:31 -0400

    fix: bottom space mobile fix

--- components/content/section/About.tsx
+++ components/content/section/About.tsx
@@ -84,7 +84,7 @@ export default function About() {
         </a>
       </div>
       {/* BOTTOM SPACER */}
-      <div className="bg-[#000] bg-opacity-0 py-44 md:py-96 lg:py-52 2xl:py-96"></div>
+      <div className="bg-[#000] bg-opacity-0 py-32 md:py-96 lg:py-52 2xl:py-96"></div>
     </>
   );
 }
