f48a4ecd4757df4c62f051402327b45b086455c1
jaavin <jaavinjm123@gmail.com>
2023-08-15 19:03:40 -0400

    chore: format

--- components/home/MotionHeader.tsx
+++ components/home/MotionHeader.tsx
@@ -2,14 +2,12 @@ import { motion, MotionValue, useTransform } from "framer-motion";
 import { Suspense, useEffect, useRef, useState } from "react";
 import Image from "next/image";
 import Content from "@/components/content/Content";
-import { Gradient} from "@/components/shared/Gradient";
-
+import { Gradient } from "@/components/shared/Gradient";
 
 const gradient = new Gradient();
 
 export default function MotionHeader(props: { value: MotionValue<number> }) {
   const MAX_GRADIENT_COLOURS = 5;
-
 
   const [height, setHeight] = useState(0);
   const elementRef = useRef<HTMLDivElement | null>(null);
@@ -61,7 +59,11 @@ export default function MotionHeader(props: { value: MotionValue<number> }) {
       >
         <Suspense fallback={null}>
           <div className="relative">
-            <canvas id="gradient-canvas" data-transition-in className={`absolute inset-[0.5px] opacity-100 gradient-colour-${currentColorIndex}`}></canvas>
+            <canvas
+              id="gradient-canvas"
+              data-transition-in
+              className={`absolute inset-[0.5px] opacity-100 gradient-colour-${currentColorIndex}`}
+            ></canvas>
             <div className="relative">
               <div>
                 <Image
