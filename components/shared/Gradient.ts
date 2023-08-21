6984e551e63a20449e9fb324dab4fa9ea3132aee
jaavin <jaavinjm123@gmail.com>
2023-08-21 17:42:38 -0400

    fix: add return types from declaration

--- components/shared/Gradient.ts
+++ components/shared/Gradient.ts
@@ -849,6 +849,24 @@ class Gradient {
       .filter(Boolean)
       .map(normalizeColor);
   }
+
+  async connect(): void;
+  disconnect(): void;
+  initMaterial(): void;
+  initMesh(): void;
+  shouldSkipFrame(e: number): boolean;
+  updateFrequency(e: number): void;
+  toggleColor(index: number): void;
+  showGradientLegend(): void;
+  hideGradientLegend(): void;
+  init(): void;
+  waitForCssVars(): void;
+  initGradientColors(): void;
+
+  initGradient(elementSelector: string): void;
+  play(): void;
+  pause(): void;
+
 }
 
 export { Gradient };
